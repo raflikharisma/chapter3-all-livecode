5d07509c4cd07fa89910065bd3fd68c2346db638
raflikharismaa <raflikharisma29@webmail.umm.ac.id>
2024-03-14 23:03:55 +0700

    feat: http method

--- materi-node/app.js
+++ materi-node/app.js
@@ -11,31 +11,102 @@ const PORT = 4000;
 //middleware
 app.use(express.json());
 
-app.get(`/`, (req, res, next) => {
+
+
+const defaultRooter = (req, res, next) => {
   res.send(`<h1>Hallooo</h1>`);
-});
-app.get(`/api/v1/data`, (req, res, next) => {
+};
+
+const getAllData = (req, res, next) => {
   res.status(200).json({
     status: "Success",
     totalData: dataDummy.length,
     data: {
       customers: dataDummy,
-      //   customers: customers,
     },
   });
-});
+}
 
-app.post(`/api/v1/`, (req, res) => {
-  const newData = req.body;
-  dataDummy.push(newData);
+const getDataById = (req, res, next) => {
+  const { id } = req.params;
+  console.log(id);
 
-  res.status(201).json({
+  const customer = customers.find((cust) => cust._id == id);
+  console.log(customer);
+
+  res.status(200).json({
     status: "Success",
     data: {
-      customer: newData,
+      customers: customer,
     },
   });
-});
+}
+
+const createData = (req, res) => {
+  const newData = req.body;
+  customers.push(newData);
+
+  fs.writeFile(`${__dirname}/data/dummy.json`, JSON.stringify(customers), (err) => {
+    res.status(201).json({
+      status: "Success",
+      data: {
+        customer: newData,
+      },
+    });
+  });
+}
+
+const updateDataById =  (req, res) => {
+  const { id } = req.params;
+
+  const customer = customers.find((cust) => cust._id == id);
+  const customerFindex = customers.findIndex((cust) => cust._id == id);
+  if (!customer) {
+    res.status(404).json({
+      status: "Failed",
+      message: "Data not found",
+    });
+  }
+
+  customers[customerFindex] = { ...customers[customerFindex], ...req.body };
+
+  fs.writeFile(`${__dirname}/data/dummy.json`, JSON.stringify(customers), (err) => {
+    res.status(200).json({
+      status: "success",
+      message: "Successfully Updating data",
+    });
+  });
+}
+
+const deleteDataById = (req, res) => {
+  const { id } = req.params;
+
+  const customer = customers.find((cust) => cust._id == id);
+  const customerFindex = customers.findIndex((cust) => cust._id == id);
+  if (!customer) {
+    res.status(404).json({
+      status: "Failed",
+      message: "Data not found",
+    });
+  }
+
+  customers.splice(customerFindex, 1);
+
+  fs.writeFile(`${__dirname}/data/dummy.json`, JSON.stringify(customers), (err) => {
+    res.status(200).json({
+      status: "success",
+      message: "Successfully Deleting data",
+    });
+  });
+}
+
+
+
+//initialize rooting
+app.route(`/api/v1/customers`).get(getAllData).post(createData);
+app.route(`/api/v1/customers/:id`).get(getDataById).patch(updateDataById).delete(deleteDataById);
+app.get('/', defaultRooter);
+
 
 app.listen(PORT, () => {
   console.log(`App Running on Port ${PORT}`);
